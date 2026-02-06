a41b1d5ed82af68cc0d1f7987a2ed04b010547fb
Raihan Uddin <uddinraihan797@gmail.com>
2026-02-06 15:13:39 +0600

    home page and autocomplete api path included

--- core/urls.py
+++ core/urls.py
@@ -14,9 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from property import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", views.home, name="home"),
+    # Autocomplete endpoint
+    path("autocomplete/", views.autocomplete_location, name="autocomplete"),
+    path("properties/", include("property.urls")),
 ]
