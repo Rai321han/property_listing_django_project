18b5ce2fd9cc5ce13d46128b6bf9818052b342a6
Raihan Uddin <uddinraihan797@gmail.com>
2026-02-10 17:13:02 +0600

    alter model

--- property/models/location.py
+++ property/models/location.py
@@ -6,7 +6,7 @@ class Location(models.Model):
     Represents a geographic location where properties can be situated.
     """
 
-    name = models.CharField(max_length=255, unique=True, db_index=True)
+    name = models.CharField(max_length=255, db_index=True)
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=100, blank=True)
     country = models.CharField(max_length=100, default="USA")
@@ -17,14 +17,20 @@ class Location(models.Model):
         ordering = ["name"]
         verbose_name = "Location"
         verbose_name_plural = "Locations"
+        constraints = [
+            models.UniqueConstraint(
+                fields=["name", "city", "country"],
+                name="unique_location_per_area",
+            )
+        ]
 
     def __str__(self):
-        return f"{self.name}, {self.city}"
+        return self.full_address
 
     @property
     def full_address(self):
-        """Return full formatted address"""
-        parts = [self.name, self.city, self.country]
+        parts = [self.name, self.city]
         if self.state:
             parts.append(self.state)
+        parts.append(self.country)
         return ", ".join(parts)
